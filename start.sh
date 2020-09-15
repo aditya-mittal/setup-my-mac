3fc094446b05042b3868208f1523b043d7e74d5c
Aditya Mittal <amittal@equalexperts.com>
2020-09-15 16:42:13 +0530

    Add code to clone setup repo

--- start.sh
+++ start.sh
@@ -10,3 +10,22 @@ curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py --user
 # install ansible
 pip install --user ansible
+
+installdir="/tmp/setup-my-mac-$RANDOM"
+mkdir $installdir
+
+#git clone https://github.com/aditya-mittal/setup-my-mac.git $installdir 
+#if [ ! -d $installdir ]; then
+#    echo "failed to find setup-my-mac."
+#    echo "git cloned failed"
+#    exit 1
+#else
+#    cd $installdir 
+#    ansible-playbook -i ./hosts playbook.yml --verbose
+#fi
+
+echo "Cleaning up..."
+
+rm -Rfv /tmp/$installdir
+
+echo "And, voila! We are done!"
