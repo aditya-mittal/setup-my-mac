b890996cb0ac984ec20dba5ac29cbd602ae4712b
Aditya Mittal <amittal@equalexperts.com>
2020-09-15 15:47:22 +0530

    Add command to install pip & ansible

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
 echo "==========================================="
 echo "Setting up your mac using setup-my-mac"
 echo "==========================================="
-
+# install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-
+# install git
 brew install git
+# install pip
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py --user
+# install ansible
+pip install --user ansible
