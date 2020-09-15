8ddaf5e199137c809f816f652ebc09d9a4c635e5
Aditya Mittal <amittal@equalexperts.com>
2020-09-15 12:42:42 +0530

    Add brew and git

---
+++ start.sh
@@ -0,0 +1,7 @@
+echo "==========================================="
+echo "Setting up your mac using setup-my-mac"
+echo "==========================================="
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+brew install git
