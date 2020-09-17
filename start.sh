3f0a166b21814df51fafe5bdb117781edc1a6a7a
Aditya Mittal <amittal@equalexperts.com>
2020-09-17 16:46:16 +0530

    Add option to uninstall and clean-up the installed softwares and tools

--- start.sh
+++ start.sh
@@ -1,6 +1,31 @@
-echo "==========================================="
+#!/bin/bash
+set -e
+
+function uninstall() {
+
+    echo "WARNING : This will remove homebrew and all applications installed through it"
+    echo -n "Are you sure you want to do that? [y/n] : "
+    read confirmation
+
+    if [ $confirmation == "y" ]; then
+        brew remove --force $(brew list)
+        brew remove --force $(brew list --cask)
+        yes  | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh)"
+        exit 0
+    else
+        echo "Keeping everything intact"
+        exit 0
+    fi
+}
+
+if [ $1 == "uninstall" ]; then
+    uninstall
+fi
+
+echo "======================================"
 echo "Setting up your mac using setup-my-mac"
-echo "==========================================="
+echo "======================================"
+
 # install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 # install git
@@ -8,16 +33,18 @@ brew install git
 # install pip
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py --user
+ln -sf ~/Library/Python/2.7/bin/pip /usr/local/bin/pip
 # install ansible
 pip install --user ansible
+ln -sf ~/Library/Python/2.7/bin/ansible* /usr/local/bin/
 
 installdir="/tmp/setup-my-mac-$RANDOM"
 mkdir $installdir
 
 #git clone https://github.com/aditya-mittal/setup-my-mac.git $installdir 
 #if [ ! -d $installdir ]; then
-#    echo "failed to find setup-my-mac."
-#    echo "git cloned failed"
+#    echo "Failed to find setup-my-mac."
+#    echo "git clone failed"
 #    exit 1
 #else
 #    cd $installdir 
