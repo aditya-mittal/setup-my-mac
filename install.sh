199afb2f956c30f31c350a8f229cb44e11863aa6
Pratik Bhawsar <82140743+pratikbhawsar26@users.noreply.github.com>
2023-03-24 16:40:38 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ brew install git
 # install pip
 curl https://bootstrap.pypa.io/pip/get-pip.py -o get-pip.py
 python3 get-pip.py --user
-ln -sf ~/Library/Python/3.9/bin/pip /usr/local/bin/pip
+ln -sf ~/Library/Python/3.9/bin/pip /usr/local/bin/
 # install ansible
 pip install --user ansible
 ln -sf ~/Library/Python/3.9/bin/ansible* /usr/local/bin/
