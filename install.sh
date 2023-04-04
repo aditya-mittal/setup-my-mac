34ec669d8fc20b4ea11c447a4b16c081b36674b9
Pratik Bhawsar <82140743+pratikbhawsar26@users.noreply.github.com>
2023-04-04 11:19:17 +0530

    Update install.sh
    Added python@3.10 installation

--- install.sh
+++ install.sh
@@ -9,13 +9,14 @@ echo "======================================"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 # install git
 brew install git
+brew install python@3.10
 # install pip
 curl https://bootstrap.pypa.io/pip/get-pip.py -o get-pip.py
 python3 get-pip.py --user
-sudo ln -sf ~/Library/Python/3.9/bin/pip /usr/local/bin/pip
+sudo ln -sf ~/Library/Python/3.10/bin/pip /usr/local/bin/pip
 # install ansible
-pip install --user ansible
-sudo ln -sf ~/Library/Python/3.9/bin/ansible* /usr/local/bin/
+pip3 install --user ansible
+sudo ln -sf ~/Library/Python/3.10/bin/ansible* /usr/local/bin/
 
 installdir="/tmp/setup-my-mac-$RANDOM"
 mkdir $installdir
