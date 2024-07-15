96e26df073a0cd7ff6fdcdb3436007a8da2a555f
Aditya Mittal <aditya.mittal@equalexperts.com>
2024-07-15 16:13:39 +0530

    Updated command to install pip

--- install.sh
+++ install.sh
@@ -15,8 +15,7 @@ echo "Git installed..."
 brew install --quiet python@3.10
 echo "Python installed..."
 # install pip
-curl https://bootstrap.pypa.io/pip/get-pip.py -o get-pip.py
-python3 get-pip.py --user
+python3 -m ensurepip --upgrade
 echo "Pip installed..."
 sudo mkdir -p /usr/local/bin
 sudo ln -sf /opt/homebrew/Cellar/python@3.10/3.10.*/bin/pip3.10 /usr/local/bin/
